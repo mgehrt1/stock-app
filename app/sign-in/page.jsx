11f91448c33960b1f01d2f9280d1ff97f2cee0e7
Will Fitzgerald <wbfitzgerald@wisc.edu>
2023-07-16 21:06:08 -0500

    Added username bar

--- app/sign-in/page.jsx
+++ app/sign-in/page.jsx
@@ -1,3 +1,20 @@
+'use client'
+
+import { useState } from "react";
+
 export default function SignIn() {
-    return <h1>Sign in page</h1>
+  const handleUserSubmit = async (e) => {
+    e.preventDefault();
+
+
+  }
+  return (
+    <div>
+      <h1>Sign in page</h1>
+        <form onSubmit={handleUserSubmit}>
+          <input type="text" name="username" placeholder="Enter Username" value="" onChange={(e) => setStock(e.target.value) } className ="border-2 border-black" />
+        </form>
+    </div>
+
+  ) 
 }
