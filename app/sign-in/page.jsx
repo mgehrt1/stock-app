cf81ff10161375890baa829545e0d23e395cabac
Will Fitzgerald <wbfitzgerald@wisc.edu>
2023-07-16 21:34:32 -0500

    Updated

--- app/sign-in/page.jsx
+++ app/sign-in/page.jsx
@@ -1,20 +1,32 @@
 'use client'
+import 'app/globals.css';
 
 import { useState } from "react";
 
 export default function SignIn() {
+  const [username, setUser] = useState("");
+  const [password, setPassword] = useState("");
+
   const handleUserSubmit = async (e) => {
     e.preventDefault();
-
+    
+    
 
   }
   return (
-    <div>
-      <h1>Sign in page</h1>
-        <form onSubmit={handleUserSubmit}>
-          <input type="text" name="username" placeholder="Enter Username" value="" onChange={(e) => setStock(e.target.value) } className ="border-2 border-black" />
-        </form>
-    </div>
+    <div className='SignInbackground'>
+    <h1 className='h1'>Username:</h1>
+      <form>
+        <input type="text" name="username" placeholder="Enter Username" value={username} onChange={(e) => setUser(e.target.value) } className ="border-2 border-black" />
+        <br></br>
+        <br></br>
+        <h1>Password:</h1>
+        <input type="password" name="password" placeholder=" Enter password" value={username} onChange={(e) => setUser(e.target.value) } className ="border-2 border-black"/>
+        <input type="submit" value="Search" className="border-2 border-black bg-gray-200 ml-2 px-2" />
+        <br></br>
+        <a className="forgotPasswordLink" href="./forgotpassword">Forgot password?</a>
+      </form>
+  </div>
 
   ) 
 }
