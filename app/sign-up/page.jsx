9d0b633cd2e5c95f198e6ede680ab872ee1c0e79
jrohrs <jrohrs@wisc.edu>
2023-07-26 07:43:03 -0500

    changes to sign up page

--- app/sign-up/page.jsx
+++ app/sign-up/page.jsx
@@ -1,3 +1,20 @@
+import 'app/globals.css';
+
 export default function SignUp() {
-    return <h1>Sign up page</h1>
+    return (
+
+        <div className='sign-up-page'>
+            <h1 className='sign-up-text'>Create a new account</h1>
+            <input className='input' type="text" placeholder='First name'></input>
+            <input className='input' type="text" placeholder='Last name'></input>
+            <input className='input' type="text" placeholder='Email'></input>
+            <br></br>
+
+            <input className='input' type='password' placeholder='Password'></input>
+            <br></br>
+            <h1 className=''>Forgot password?</h1>
+            <br></br>
+            <button className='sign-up-btn'><a href="C:/stock-app-1/app/home.jsx">Sign up</a></button>
+        </div>
+    );
 }
